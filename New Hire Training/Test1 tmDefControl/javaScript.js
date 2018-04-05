189c44ebe3acf7b37fb4b253eb4adda8501a7863
robertmayeight <35906720+robertmayeight@users.noreply.github.com>
2018-04-05 19:23:18 -0400

    slide

--- New Hire Training/Test1 tmDefControl/javaScript.js
+++ New Hire Training/Test1 tmDefControl/javaScript.js
@@ -135,7 +135,7 @@ function clickZoomDown(){
 	TweenMax.to(showWindow, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
 }
 
-showWindow.addEventListener('gesturestart', function(e) {
+showWindow.addEventListener('gestureend', function(e) {
     if (e.scale < 1.0) {
         scaleUp = scaleUp + .1;
 	TweenMax.to(showWindow, .00, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
