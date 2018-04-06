73b48211aeb47bcb2e5812dc041d7b260d8c177a
robertmayeight <35906720+robertmayeight@users.noreply.github.com>
2018-04-06 12:24:10 -0400

    update electronic defrost

--- New Hire Training/Test1 tmDefControl/javaScript.js
+++ New Hire Training/Test1 tmDefControl/javaScript.js
@@ -5,30 +5,28 @@ xhr.send("");
 var schematic = document.getElementById("showWindow").appendChild(xhr.responseXML.documentElement);
 schematic.classList.add("center");
 
-var noPaths = document.getElementById("diagram1").getElementsByTagName("path")
+var noPaths = document.getElementById("diagram1").getElementsByTagName("path");
 var noPathsLength = noPaths.length;
 for(i=0; i<noPathsLength; i++){
 	var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-		path.setAttribute('stroke','blue');
-		path.setAttribute('fill','none');
-		path.setAttribute('opacity',0);
-		path.setAttribute('id',noPaths[i].id + 'copy');
-		path.setAttribute('onclick','wireClicked(this);');
-		path.setAttribute('onmouseover','this.style.cursor = "default"; overWire(this);');
-
-		path.setAttribute('onmouseout','outWire(this);');
-		path.style['stroke-width']=1;
-		path.style['stroke-linecap']="round";
-		noPaths[i].style['stroke-linecap']="round";
-		path.setAttribute("d", noPaths[i].getAttribute("d"))
-		diagram1.appendChild(path);	
-		path.style["stroke-width"]= 3;
-		path.setAttribute('touchmove','wireClicked(this);');	
+	path.setAttribute('stroke','blue');
+	path.setAttribute('fill','none');
+	path.setAttribute('opacity',0);
+	path.setAttribute('id',noPaths[i].id + 'copy');
+	path.setAttribute('onclick','wireClicked(this);');
+	path.setAttribute('onmouseover','this.style.cursor = "default"; overWire(this);');
+	path.setAttribute('onmouseout','outWire(this);');
+	path.style['stroke-width']=1;
+	path.style['stroke-linecap']="round";
+	noPaths[i].style['stroke-linecap']="round";
+	path.setAttribute("d", noPaths[i].getAttribute("d"));
+	diagram1.appendChild(path);
+	path.style["stroke-width"]= 3;
+	path.setAttribute('touchmove','wireClicked(this);');	
 }
 
 var schematicDrag = Draggable.create(showWindow, {zIndexBoost:false});
-showWindow.addEventListener("DOMMouseScroll", function(e){zoomSchematic(e)}, false)
-
+showWindow.addEventListener("DOMMouseScroll", function(e){zoomSchematic(e)}, false);
 
 var scaleUp = 1;
 function zoomSchematic(e){
@@ -48,24 +46,20 @@ function zoomSchematic(e){
 }
 
 function wireClicked(wire){
-	nameSplit = wire.id.split("copy")
+	nameSplit = wire.id.split("copy");
 	wire2 = document.getElementById(nameSplit[0]);
-	TweenMax.to(wire2,.1,{stroke:highlightColor})
+	TweenMax.to(wire2,.1,{stroke:highlightColor});
 }
 
 function overWire(wire){
-	nameSplit = wire.id.split("copy")
-	// document.getElementById(nameSplit[0]).setAttribute("opacity", ".25"); 
+	nameSplit = wire.id.split("copy");
 	wire.setAttribute("opacity", ".5"); 
 }
 
 function outWire(wire){
-nameSplit = wire.id.split("copy")
-	// document.getElementById(nameSplit[0]).setAttribute("opacity", "1");
+nameSplit = wire.id.split("copy");
 	wire.setAttribute("opacity", "0");
 }
-
-
 
 var highlightColor=black;
 function changeColors(myColor){
@@ -79,7 +73,6 @@ function changeColors(myColor){
 	blackBox.style.border = "none";
 	brownBox.style.border = "none";
 	highlightColor=myColor;
-
 	switch(myColor) {
     case "red":
     redBox.style.border = "medium solid #000000";
@@ -110,18 +103,15 @@ function changeColors(myColor){
     case "black":
     blackBox.style.border = "medium solid #000000";
     break;
-}
+	}
 }
 
-// zoomSlider.value = "0";
 zoomSlider.addEventListener("input", update);
 function update(){
-	console.log(zoomSlider.value)
 	TweenMax.set(showWindow, {scaleX:zoomSlider.value, scaleY:zoomSlider.value, transformOrigin: "50% 50%", ease: Power0.easeNone});
 }
 
-
-
+// -----------------------------------------------------------------------------------------------------------------------------
 var ccSwitchRotated=false;
 ccSwitch.setAttribute('onclick','changeccSwitch();');
 ccSwitch.setAttribute('onmouseover','this.style.cursor = "pointer";');
@@ -132,13 +122,12 @@ function changeccSwitch(){
 		TweenMax.to(g3942,1,{y:"-=6"});
 		ccSwitchRotated=true;
 	}else{
-		TweenMax.to(tcBlade,1,{rotation:0})
-		TweenMax.to(tcBladecopy,1,{rotation:0})
+		TweenMax.to(tcBlade,1,{rotation:0});
+		TweenMax.to(tcBladecopy,1,{rotation:0});
 		TweenMax.to(g3942,1,{y:"+=7"});
 		ccSwitchRotated=false;
 	}
 }
-
 
 var timerSwitchRotated=false;
 timerSwitch.setAttribute('onclick','changeTimerSwitch();');
@@ -146,34 +135,32 @@ timerSwitch.setAttribute('onmouseover','this.style.cursor = "pointer";');
 
 function changeTimerSwitch(){
 	if(timerSwitchRotated === false){
-		TweenMax.to(defrostControlWiper,1,{rotation:-33})
-		TweenMax.to(defrostControlWipercopy,1,{rotation:-33})
+		TweenMax.to(defrostControlWiper,1,{rotation:-33});
+		TweenMax.to(defrostControlWipercopy,1,{rotation:-33});
 		timerSwitchRotated=true;
 	}else{
-		TweenMax.to(defrostControlWiper,1,{rotation:0})
-		TweenMax.to(defrostControlWipercopy,1,{rotation:0})
+		TweenMax.to(defrostControlWiper,1,{rotation:0});
+		TweenMax.to(defrostControlWipercopy,1,{rotation:0});
 		timerSwitchRotated=false;
 	}
 }
 
-//Door Switch Rotation
 var doorSwitchRotated=false;
 doorSwitch.setAttribute('onclick','changeDoorSwitch();');
 doorSwitch.setAttribute('onmouseover','this.style.cursor = "pointer";');
 
 function changeDoorSwitch(){
 	if(doorSwitchRotated === false){
-		TweenMax.to(path200,1,{rotation:-26})
-		TweenMax.to(path200copy,1,{rotation:-26})
+		TweenMax.to(path200,1,{rotation:-26});
+		TweenMax.to(path200copy,1,{rotation:-26});
 		doorSwitchRotated=true;
 	}else{
-		TweenMax.to(path200,1,{rotation:0})
-		TweenMax.to(path200copy,1,{rotation:0})
+		TweenMax.to(path200,1,{rotation:0});
+		TweenMax.to(path200copy,1,{rotation:0});
 		doorSwitchRotated=false;
 	}
 }
 
-//Defrost Thermostat Switch Rotation
 var defrostThermostatRotated=false;
 defrostThermostat.setAttribute('onclick','changeDefrostThermostatSwitch();');
 defrostThermostat.setAttribute('onmouseover','this.style.cursor = "pointer";');
@@ -184,8 +171,8 @@ function changeDefrostThermostatSwitch(){
 		TweenMax.to(path324,1,{y:3});
 		defrostThermostatRotated=true;
 	}else{
-		TweenMax.to(path334,1,{rotation:0})
-		TweenMax.to(path334copy,1,{rotation:0})
+		TweenMax.to(path334,1,{rotation:0});
+		TweenMax.to(path334copy,1,{rotation:0});
 		TweenMax.to(path324,1,{y:0});
 		defrostThermostatRotated=false;
 	}
