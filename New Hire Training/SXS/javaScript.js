4ceee28a0d3baf6283e4e1044855da3dc51dfb95
robertmayeight <35906720+robertmayeight@users.noreply.github.com>
2018-04-06 14:13:27 -0400

    update sxs

--- New Hire Training/SXS/javaScript.js
+++ New Hire Training/SXS/javaScript.js
@@ -5,38 +5,28 @@ xhr.send("");
 var schematic = document.getElementById("showWindow").appendChild(xhr.responseXML.documentElement);
 schematic.classList.add("center");
 
-var noPaths = document.getElementById("diagram1").getElementsByTagName("path")
+var noPaths = document.getElementById("diagram1").getElementsByTagName("path");
 var noPathsLength = noPaths.length;
 for(i=0; i<noPathsLength; i++){
 	var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-		path.setAttribute('stroke','blue');
-		path.setAttribute('fill','none');
-		path.setAttribute('opacity',0);
-		path.setAttribute('id',noPaths[i].id + 'copy');
-		path.setAttribute('onclick','wireClicked(this);');
-		path.setAttribute('onmouseover','this.style.cursor = "default"; overWire(this);');
-		path.setAttribute('onmouseout','outWire(this);');
-		path.style['stroke-width']=1;
-		path.style['stroke-linecap']="round";
-		noPaths[i].style['stroke-linecap']="round";
-		path.setAttribute("d", noPaths[i].getAttribute("d"))
-		diagram1.appendChild(path);	
-		path.style["stroke-width"]= 3;	
+	path.setAttribute('stroke','blue');
+	path.setAttribute('fill','none');
+	path.setAttribute('opacity',0);
+	path.setAttribute('id',noPaths[i].id + 'copy');
+	path.setAttribute('onclick','wireClicked(this);');
+	path.setAttribute('onmouseover','this.style.cursor = "default"; overWire(this);');
+	path.setAttribute('onmouseout','outWire(this);');
+	path.style['stroke-width']=1;
+	path.style['stroke-linecap']="round";
+	noPaths[i].style['stroke-linecap']="round";
+	path.setAttribute("d", noPaths[i].getAttribute("d"));
+	diagram1.appendChild(path);
+	path.style["stroke-width"]= 3;
+	path.setAttribute('touchmove','wireClicked(this);');	
 }
 
-var schematicDrag = Draggable.create(showWindow, {zIndexBoost:false});
-showWindow.addEventListener("DOMMouseScroll", function(e){zoomSchematic(e)}, false)
-
-window.addEventListener("keydown", keyBoardZoom, false);
-function keyBoardZoom(e) {
-    if (e.keyCode == 38) {
-       scaleUp = scaleUp + .025;
-	TweenMax.to(showWindow, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
-   }else if (e.keyCode == 40) {
-       scaleUp = scaleUp - .05;
-	TweenMax.to(showWindow, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
-   }
-}
+var schematicDrag = Draggable.create(schematic, {zIndexBoost:false});
+schematic.addEventListener("DOMMouseScroll", function(e){zoomSchematic(e)}, false);
 
 var scaleUp = 1;
 function zoomSchematic(e){
@@ -44,32 +34,35 @@ function zoomSchematic(e){
 	switch(e.detail>0) {
 		case true:
 		if(scaleUp > .5 ){
-			scaleUp = scaleUp - .1;
-			TweenMax.to(showWindow, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
+			scaleUp = scaleUp - .25;
+			TweenMax.to(schematic, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
 		}
 		break;
 		case false:
-		scaleUp = scaleUp + .1;
-    	TweenMax.to(showWindow, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
+		scaleUp = scaleUp + .25;
+    	TweenMax.to(schematic, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
         break;
     }
 }
 
+zoomSlider.addEventListener("input", update);
+function update(){
+	TweenMax.set(schematic, {scaleX:zoomSlider.value, scaleY:zoomSlider.value, transformOrigin: "50% 50%", ease: Power0.easeNone});
+}
+
 function wireClicked(wire){
-	nameSplit = wire.id.split("copy")
+	nameSplit = wire.id.split("copy");
 	wire2 = document.getElementById(nameSplit[0]);
-	TweenMax.to(wire2,.1,{stroke:highlightColor})
+	TweenMax.to(wire2,.1,{stroke:highlightColor});
 }
 
 function overWire(wire){
-	nameSplit = wire.id.split("copy")
-	// document.getElementById(nameSplit[0]).setAttribute("opacity", ".25"); 
+	nameSplit = wire.id.split("copy");
 	wire.setAttribute("opacity", ".5"); 
 }
 
 function outWire(wire){
-nameSplit = wire.id.split("copy")
-	// document.getElementById(nameSplit[0]).setAttribute("opacity", "1");
+nameSplit = wire.id.split("copy");
 	wire.setAttribute("opacity", "0");
 }
 
@@ -85,7 +78,6 @@ function changeColors(myColor){
 	blackBox.style.border = "none";
 	brownBox.style.border = "none";
 	highlightColor=myColor;
-
 	switch(myColor) {
     case "red":
     redBox.style.border = "medium solid #000000";
@@ -107,7 +99,7 @@ function changeColors(myColor){
     break;
     case "grey":
     greyBox.style.border = "medium solid #000000";
-    highlightColor="#A9A9A9";
+    highlightColor="#f2f4f4";
     break;
     case "brown":
     brownBox.style.border = "medium solid #000000";
@@ -116,19 +108,10 @@ function changeColors(myColor){
     case "black":
     blackBox.style.border = "medium solid #000000";
     break;
-}
-}
-
-
-function clickZoomUP(){
-	scaleUp = scaleUp + .05;
-	TweenMax.to(showWindow, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
+	}
 }
 
-function clickZoomDown(){
-	scaleUp = scaleUp - .05;
-	TweenMax.to(showWindow, .5, {scaleX:scaleUp, scaleY:scaleUp, transformOrigin: "50% 50%", ease: Power0.easeNone});
-}
+// -----------------------------------------------------------------------------------------------------------------------------
 
 var highLimitSwitchRotated=false;
 highLimitSwitch.setAttribute('onclick','changeHighLimitSwitch();');
