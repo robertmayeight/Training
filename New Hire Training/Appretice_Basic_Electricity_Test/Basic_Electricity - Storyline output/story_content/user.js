7f3172824115b45253509795ba3e7849fc0a6eb7
robertmayeight <35906720+robertmayeight@users.noreply.github.com>
2018-12-11 20:53:49 -0500

    Javascript

--- New Hire Training/Appretice_Basic_Electricity_Test/Basic_Electricity - Storyline output/story_content/user.js
+++ New Hire Training/Appretice_Basic_Electricity_Test/Basic_Electricity - Storyline output/story_content/user.js
@@ -1,1 +1,3 @@
-alert("hello")
+alert("LOADED");
+var slideAudio = document.getElementById('music');
+slideAudio.src="https://robertmayeight.github.io/Training/New%20Hire%20Training/Appretice_Basic_Electricity_Test/slide3/slide3Audio.ogg"// User defined code goes here
