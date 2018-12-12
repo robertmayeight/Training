1e9eaa58bd80536680fefdd4a48af1bcb7c710c4
robertmayeight <35906720+robertmayeight@users.noreply.github.com>
2018-12-11 21:14:18 -0500

    J

--- New Hire Training/Appretice_Basic_Electricity_Test/Basic_Electricity - Storyline output/story_content/user.js
+++ New Hire Training/Appretice_Basic_Electricity_Test/Basic_Electricity - Storyline output/story_content/user.js
@@ -1,4 +1,4 @@
-alert("LOADED 3");
+alert("LOADED 4");
 
 var slide1AudioTrack ="https://robertmayeight.github.io/Training/New%20Hire%20Training/Appretice_Basic_Electricity_Test/slide2/slide2Audio.ogg";
 var slide2AudioTrack = "https://robertmayeight.github.io/Training/New%20Hire%20Training/Appretice_Basic_Electricity_Test/slide3/slide3Audio.ogg";
