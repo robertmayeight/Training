3c41921a10c8d99e942d521e64ce1ca16c38b27b
robertmayeight <35906720+robertmayeight@users.noreply.github.com>
2018-12-11 21:09:26 -0500

    J

--- New Hire Training/Appretice_Basic_Electricity_Test/Basic_Electricity - Storyline output/story_content/user.js
+++ New Hire Training/Appretice_Basic_Electricity_Test/Basic_Electricity - Storyline output/story_content/user.js
@@ -1,4 +1,4 @@
-alert("LOADED 2");
-var slideAudio = document.getElementById('music');
-slideAudio.src="https://robertmayeight.github.io/Training/New%20Hire%20Training/Appretice_Basic_Electricity_Test/slide2/slide2Audio.ogg";
-slideAudio.src="https://robertmayeight.github.io/Training/New%20Hire%20Training/Appretice_Basic_Electricity_Test/slide3/slide3Audio.ogg";
+alert("LOADED 3");
+
+var slide1AudioTrack = slideAudio.src="https://robertmayeight.github.io/Training/New%20Hire%20Training/Appretice_Basic_Electricity_Test/slide2/slide2Audio.ogg";
+var slide2AudioTrack = slideAudio.src="https://robertmayeight.github.io/Training/New%20Hire%20Training/Appretice_Basic_Electricity_Test/slide3/slide3Audio.ogg";
