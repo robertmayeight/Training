ca96220b953b3e172562bd9cc89763c312e1d883
robertmayeight <35906720+robertmayeight@users.noreply.github.com>
2018-12-16 13:31:12 -0500

    M

--- New Hire Training/Apprentice_Basic_Electricity_Test/Basic_Electricity - Storyline output/story_content/user.js
+++ New Hire Training/Apprentice_Basic_Electricity_Test/Basic_Electricity - Storyline output/story_content/user.js
@@ -1,18 +1,1 @@
-function ExecuteScript(strId)
-{
-  switch (strId)
-  {
-      case "6KvWT8MA2LU":
-        Script1();
-        break;
-  }
-}
-
-function Script1()
-{
-  var rob = document.getElementById("line_1")
-
-alert("line" + rob.id)
-
-}
-
+// User defined code goes here
