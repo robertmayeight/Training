657c420d13c7f4ee4b99c7115751bbba5e2934a9
robertmayeight <35906720+robertmayeight@users.noreply.github.com>
2018-12-19 09:08:37 -0500

    M

--- New Hire Training/Apprentice_Basic_Electricity_Test/Basic_Electricity - Storyline output/story_content/WebObjects/6CFDFOfpWTo/slide2Js.js
+++ New Hire Training/Apprentice_Basic_Electricity_Test/Basic_Electricity - Storyline output/story_content/WebObjects/6CFDFOfpWTo/slide2Js.js
@@ -1,3 +1,28 @@
+// Opera 8.0+
+var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+// Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
+
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+// Edge 20+
+var isEdge = !isIE && !!window.StyleMedia;
+
+// Chrome 1 - 68
+var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+// Blink engine detection
+var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+
+
+console.log(isFirefox)
+
 var gArray = document.getElementsByTagName("g");
 var tArray = document.getElementsByTagName("text");
 var imageArray = document.getElementsByTagName("image");
@@ -57,6 +82,16 @@ slideAudio.ontimeupdate = function() {
 
 function playAudio(){
 	slideAudio.play();
+}
+
+if(isFirefox == true){
+	TweenLite.to(slideAudio, 1, {bottom:0})
+}
+if(isEdge == true){
+	TweenLite.to(slideAudio, 1, {bottom:-490})
+}
+if(isChrome == true){
+	TweenLite.to(slideAudio, 1, {bottom:-750})
 }
 //End Audio
 
