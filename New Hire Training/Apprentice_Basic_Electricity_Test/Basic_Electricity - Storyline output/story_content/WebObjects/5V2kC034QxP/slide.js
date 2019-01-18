477a0a120951eebcfbe0388b0b4d2e6273e301c7
robertmayeight <35906720+robertmayeight@users.noreply.github.com>
2019-01-18 14:14:39 -0500

    black cover

--- New Hire Training/Apprentice_Basic_Electricity_Test/Basic_Electricity - Storyline output/story_content/WebObjects/5V2kC034QxP/slide.js
+++ New Hire Training/Apprentice_Basic_Electricity_Test/Basic_Electricity - Storyline output/story_content/WebObjects/5V2kC034QxP/slide.js
@@ -1,8 +1,8 @@
-slide = new XMLHttpRequest();
-slide.open("GET","slide.svg",false);
-slide.overrideMimeType("image/svg+xml");
-slide.send("");
-var slide= document.getElementById("main").appendChild(slide.responseXML.documentElement);
+// slide = new XMLHttpRequest();
+// slide.open("GET","slide.svg",false);
+// slide.overrideMimeType("image/svg+xml");
+// slide.send("");
+// var slide= document.getElementById("main").appendChild(slide.responseXML.documentElement);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var svgWindow = document.getElementById("main");
