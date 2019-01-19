b792cbfcd63fdf97066d347686eb3f93ae2c6645
robertmayeight <35906720+robertmayeight@users.noreply.github.com>
2019-01-19 17:50:09 -0500

    J

--- New Hire Training/Apprentice_Basic_Electricity_Test/slide2/slide.js
+++ New Hire Training/Apprentice_Basic_Electricity_Test/slide2/slide.js
@@ -4,17 +4,17 @@
 // slide.send("");
 // var slide= document.getElementById("main").appendChild(slide.responseXML.documentElement);
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-var svgWindow = document.getElementById("main");
-var svg = d3.select(svgContent);
-function redraw(){
-	var width = svgWindow.clientWidth;
-	var height = svgWindow.clientHeight;
-	svg
-	.attr("width", width)
-	 .attr("height", height);
-}
-redraw();
-window.addEventListener("resize", redraw);
+// var svgWindow = document.getElementById("main");
+// var svg = d3.select(svgContent);
+// function redraw(){
+// 	var width = svgWindow.clientWidth;
+// 	var height = svgWindow.clientHeight;
+// 	svg
+// 	.attr("width", width)
+// 	 .attr("height", height);
+// }
+// redraw();
+// window.addEventListener("resize", redraw);
 
 //Browser Adjustments
 var slideAudio = document.getElementById('music');
